a3a0c50194b326fcebeda8b46570b46b521196dc
paulthegreat <paulthegreat@users.noreply.github.com>
2019-04-06 01:24:14 -0500

    basic game service and player functions

--- services/server/game/game_player.lua
+++ services/server/game/game_player.lua
@@ -13,4 +13,50 @@ function GamePlayer:add_player(game_options)
    self.__saved_variables:mark_changed()
 end
 
+function GamePlayer:get_gold()
+   return self._sv.gold
+end
+
+function GamePlayer:add_gold(amount)
+   if amount and amount > 0 then
+      self._sv.gold = self._sv.gold + amount
+      self.__saved_variables:mark_changed()
+   end
+end
+
+-- takes as much as possible up to amount and returns the amount taken
+function GamePlayer:take_gold(amount)
+   local gold = self._sv.gold
+   amount = math.max(0, math.min(amount, gold))
+   
+   if amount > 0 then
+      self._sv.gold = gold - amount
+      self.__saved_variables:mark_changed()
+   end
+
+   return amount
+end
+
+-- only_self if cannot spend from common player
+function GamePlayer:spend_gold(amount, only_self)
+   local gold = self._sv.gold
+   local success
+
+   if amount <= gold then
+      success = true
+   elseif not only_self then
+      -- check the common player to see if we can take enough from them
+      local common_player = tower_defense.game:get_common_player()
+      success = common_player:spend_gold(amount - gold, true)
+      amount = gold
+   end
+
+   if success then
+      self._sv.gold = gold - amount
+      self.__saved_variables:mark_changed()
+   end
+
+   return success
+end
+
 return GamePlayer
--- services/server/game/game_service.lua
+++ services/server/game/game_service.lua
@@ -1,5 +1,7 @@
 GameService = class()
 
+local COMMON_PLAYER = 'common_player'
+
 function GameService:initialize()
    self._sv = self.__saved_variables:get_data()
 
@@ -10,6 +12,50 @@ function GameService:initialize()
    if not self._sv.wave then
       self._sv.wave = 0
    end
+
+   if self._sv.countdown_timer then
+      self._sv.countdown_timer:bind(function()
+         self:_start_round()
+      end)
+   end
+end
+
+function GameService:start()
+   self:_end_of_round()
+end
+
+function GameService:destroy()
+   self:_destroy_countdown_timer()
+end
+
+function GameService:_end_of_round()
+   local countdown = radiant.util.get_config('round_countdown', '10m')
+   if radiant.util.get_config('pause_at_end_of_round', true) then
+      stonehearth.game_speed:set_game_speed(0, false)
+   end
+
+   self:_destroy_countdown_timer()
+   self:_create_countdown_timer(countdown)
+end
+
+function GameService:_destroy_countdown_timer()
+   if self._sv.countdown_timer then
+      self._sv.countdown_timer:destroy()
+      self._sv.countdown_timer = nil
+   end
+end
+
+function GameService:_create_countdown_timer(countdown)
+   self._sv.countdown_timer = stonehearth.calendar:set_persistent_timer('next wave countdown', countdown, function()
+      self:_start_round()
+   end)
+end
+
+function GameService:_start_round()
+   self:_destroy_countdown_timer()
+
+   self._sv.wave = self._sv.wave + 1
+   self.__saved_variables:mark_changed()
 end
 
 function GameService:add_player(player_id, game_options)
@@ -31,12 +77,32 @@ function GameService:add_player(player_id, game_options)
    self._sv.players[player_id] = radiant.create_controller('tower_defense:game_player', player_id, game_options)
 
    if not self._sv.common_player then
-      self._sv.common_player = radiant.create_controller('tower_defense:game_player', 'common_player', game_options)
+      self._sv.common_player = radiant.create_controller('tower_defense:game_player', COMMON_PLAYER, game_options)
    else
       self._sv.common_player:add_player(game_options)
    end
 
    self.__saved_variables:mark_changed()
+end
+
+function GameService:get_player(player_id)
+   return self._sv.players[player_id]
+end
+
+function GameService:get_common_player()
+   return self._sv.common_player
+end
+
+-- gold can only be given to the common player
+function GameService:give_gold(player_id, amount)
+   if player_id ~= COMMON_PLAYER then
+      local player = self._sv.players[player_id]
+      local common_player = self._sv.common_player
+      if player and common_player then
+         amount = player:take_gold(amount)
+         common_player:add_gold(amount)
+      end
+   end
 end
 
 return GameService
