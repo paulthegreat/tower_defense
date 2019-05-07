90890e07fdbd898f682db4848dacd4793d2ab98d
paulthegreat <paulthegreat@users.noreply.github.com>
2019-05-06 22:44:47 -0500

    made ground presences destroy on wave end, tweaked mangrove (and plant growth buff) model origin

--- data/buffs/tower/plant_growth/plant_growth.lua
+++ data/buffs/tower/plant_growth/plant_growth.lua
@@ -2,7 +2,9 @@ local PlantGrowthBuffScript = class()
 
 function PlantGrowthBuffScript:on_buff_added(entity, buff)
    local orig_render_info = radiant.entities.get_component_data(entity:get_uri(), 'render_info')
+   local orig_mob = radiant.entities.get_component_data(entity:get_uri(), 'mob')
    self._orig_scale = orig_render_info and orig_render_info.scale
+   self._orig_origin = orig_mob and orig_mob.model_origin
 
    self:_update_scale(entity, buff)
 end
@@ -13,10 +15,16 @@ function PlantGrowthBuffScript:on_repeat_add(entity, buff)
 end
 
 function PlantGrowthBuffScript:_update_scale(entity, buff)
+   local stacks = buff:get_stacks()
+   local mult = (1 + stacks * 0.2)
+
    if self._orig_scale then
-      local stacks = buff:get_stacks()
-      local new_scale = self._orig_scale * (1 + stacks * 0.2)
+      local new_scale = self._orig_scale * mult
       entity:add_component('render_info'):set_scale(new_scale)
+   end
+   if self._orig_origin then
+      local new_origin = radiant.util.to_point3(self._orig_origin):scaled(mult)
+      entity:add_component('mob'):set_model_origin(new_origin)
    end
 end
 
